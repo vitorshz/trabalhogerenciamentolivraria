72126ff84b3a3d8823eddd9e4d9d99648fb66150
Folquini <vitor.folquini@edu.unipar.br>
2023-04-16 17:54:44 -0300

    Criando class cliente
    class clientes e métodos

--- trabalholivraria/src/main/java/br/unipar/trabalholivraria/model/Clientes.java
+++ trabalholivraria/src/main/java/br/unipar/trabalholivraria/model/Clientes.java
@@ -1,13 +1,48 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package br.unipar.trabalholivraria.model;
 
-/**
- *
- * @author vinid
- */
+import java.util.ArrayList;
+
 public class Clientes {
+    private String clienteCpf;
+    private String clienteNome;
+    private ArrayList<Endereco> enderecos;
     
+    public Clientes(){
+    }
+    
+    public Clientes(String clienteCpf, String clienteNome, ArrayList<Endereco> enderecos){
+        this.clienteCpf = clienteCpf;
+        this.clienteNome = clienteNome;
+        this.enderecos = enderecos;
+    }
+
+    public String getClienteCpf() {
+        return clienteCpf;
+    }
+
+    public void setClienteCpf(String clienteCpf) {
+        this.clienteCpf = clienteCpf;
+    }
+
+    public String getClienteNome() {
+        return clienteNome;
+    }
+
+    public void setClienteNome(String clienteNome) {
+        this.clienteNome = clienteNome;
+    }
+
+    public ArrayList<Endereco> getEnderecos() {
+        return enderecos;
+    }
+
+    public void setEnderecos(ArrayList<Endereco> enderecos) {
+        this.enderecos = enderecos;
+    }
+
+    @Override
+    public String toString() {
+        return "Clientes{" + "clienteCpf=" + clienteCpf + ", clienteNome=" + clienteNome + ", enderecos=" + enderecos + '}';
+    }
+
 }
