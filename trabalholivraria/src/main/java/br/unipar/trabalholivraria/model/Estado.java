42374be76489a751cae7db2c95e1e5e7ac9f2137
Folquini <vitor.folquini@edu.unipar.br>
2023-04-16 17:36:10 -0300

    Adicionando classes cidade, endereco, estado e pais
    criação de classes com métodos

--- trabalholivraria/src/main/java/br/unipar/trabalholivraria/model/Estado.java
+++ trabalholivraria/src/main/java/br/unipar/trabalholivraria/model/Estado.java
@@ -1,13 +1,58 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package br.unipar.trabalholivraria.model;
 
-/**
- *
- * @author vinid
- */
+import java.util.ArrayList;
+
 public class Estado {
+    private int codEstado;
+    private String nomeEstado;
+    private String siglaUF;
+    private ArrayList<Pais> paises;
     
+    public Estado(){
+    }
+    
+    public Estado(int codEstado, String nomeEstado, String siglaUF, ArrayList<Pais> paises){
+        this.codEstado = codEstado;
+        this.nomeEstado = nomeEstado;
+        this.paises = paises;
+        this.siglaUF = siglaUF;
+    }
+
+    public int getCodEstado() {
+        return codEstado;
+    }
+
+    public void setCodEstado(int codEstado) {
+        this.codEstado = codEstado;
+    }
+
+    public String getNomeEstado() {
+        return nomeEstado;
+    }
+
+    public void setNomeEstado(String nomeEstado) {
+        this.nomeEstado = nomeEstado;
+    }
+
+    public String getSiglaUF() {
+        return siglaUF;
+    }
+
+    public void setSiglaUF(String siglaUF) {
+        this.siglaUF = siglaUF;
+    }
+
+    public ArrayList<Pais> getPaises() {
+        return paises;
+    }
+
+    public void setPaises(ArrayList<Pais> paises) {
+        this.paises = paises;
+    }
+
+    @Override
+    public String toString() {
+        return "Estado{" + "codEstado=" + codEstado + ", nomeEstado=" + nomeEstado + ", siglaUF=" + siglaUF + ", paises=" + paises + '}';
+    }
 }
+
