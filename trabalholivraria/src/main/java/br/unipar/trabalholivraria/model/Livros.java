564283ba0aa47497d0079896a0ecfac537cf7f48
vitorshz <110640686+vitorshz@users.noreply.github.com>
2023-04-17 14:00:54 -0300

    Class Livros

--- trabalholivraria/src/main/java/br/unipar/trabalholivraria/model/Livros.java
+++ trabalholivraria/src/main/java/br/unipar/trabalholivraria/model/Livros.java
@@ -1,15 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package br.unipar.trabalholivraria.model;
 
 import java.util.ArrayList;
 
-/**
- *
- * @author vinid
- */
 public class Livros {
     private int cod_livro;
     private String tituloLivro;
