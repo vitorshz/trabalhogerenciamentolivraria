d97f783fbfdda137769b1f102be72a72f3ee4f1d
vitorshz <110640686+vitorshz@users.noreply.github.com>
2023-04-17 13:59:24 -0300

    Class editora

--- trabalholivraria/src/main/java/br/unipar/trabalholivraria/model/Editora.java
+++ trabalholivraria/src/main/java/br/unipar/trabalholivraria/model/Editora.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package br.unipar.trabalholivraria.model;
 
-/**
- *
- * @author vinid
- */
+
 public class Editora {
     private int cod_editora;
     private String nmEditora;
@@ -42,5 +36,4 @@ public class Editora {
     public String toString() {
         return "Editora{" + "cod_editora=" + cod_editora + ", nmEditora=" + nmEditora + ", cnpjEditora=" + cnpjEditora + '}';
     }
-    
 }
