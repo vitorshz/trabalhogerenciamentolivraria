3196e89f201e47118cd7f84a07566c820c15f874
vitorshz <110640686+vitorshz@users.noreply.github.com>
2023-04-17 14:00:24 -0300

    Class generos

--- trabalholivraria/src/main/java/br/unipar/trabalholivraria/model/Generos.java
+++ trabalholivraria/src/main/java/br/unipar/trabalholivraria/model/Generos.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package br.unipar.trabalholivraria.model;
 
-/**
- *
- * @author vinid
- */
+
 public class Generos {
     private int cod_generos;
     private String dsGeneros;
